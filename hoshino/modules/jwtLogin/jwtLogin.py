d2d95c212502334bf1450e15f170510c41ccf2f4
yiyione <yiyi_one@foxmail.com>
2020-06-27 03:29:48 +0800

    fix

--- hoshino/modules/jwtLogin/jwtLogin.py
+++ hoshino/modules/jwtLogin/jwtLogin.py
@@ -15,9 +15,9 @@ def get_config():
 def generateToken(ctx, ignore_super_user=False):
     user_id = ctx['user_id']
     config = get_config()
-    return jwt.encode({'uid': user_id}, config['secret'], algorithm='HS256')
+    return jwt.encode({'uid': user_id}, config['secret'], algorithm='HS256').decode('utf-8')
 
-@sv.on_command('token', aliases=('登录凭证', '密码'), only_to_me=True)
+@on_command('token', aliases=('登录凭证'), only_to_me=True)
 async def getToken(session):
     if session.ctx['message_type'] == 'group':
         await session.send('请私聊机器人token,以获取web登录凭证。')
